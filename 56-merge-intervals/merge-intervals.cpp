6af2f4edfa74fc39232202a6c502664e77c31851
Dakshita Sharma <dakshita5002@gmail.com>
2026-06-12 18:53:19 +0530

    Time: 4 ms (65.95%) | Memory: 23.9 MB (58.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -7,7 +7,7 @@ class Solution{
         vector<vector<int>> ans;
 
         for(int i=0; i<n; i++){
-            if(ans.empty() || intervals[i][0]>ans.back()[1]){
+            if(ans.size()==0 || intervals[i][0]>ans.back()[1]){
                 ans.push_back(intervals[i]);
             }
             else{
