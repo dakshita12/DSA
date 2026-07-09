457b55560c8fe911e7e26999ec9906c4222c362e
Dakshita Sharma <dakshita5002@gmail.com>
2026-07-09 23:51:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (83.42%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -3,9 +3,9 @@ public:
     int squareSum(int n){
         int sum = 0;
         while(n>0){
-            int d = n%10;
-            sum = sum + d*d;
-            n = n/10;
+            int d = n%10;    // Extract the last digit
+            sum = sum + d*d; // Add its square to the sum
+            n = n/10;        // Remove the last digit
         }
         return sum;
     }
@@ -13,10 +13,13 @@ public:
     bool isHappy(int n) {
         int slow = n;
         int fast = n;
+
         while(fast != 1){
             slow = squareSum(slow);
             fast = squareSum(fast);
             fast = squareSum(fast);
+
+            // If both pointers meet at a no other than 1, cycle exists, the number is not happy.
             if(slow==fast && slow!=1){
                 return false;
             }
